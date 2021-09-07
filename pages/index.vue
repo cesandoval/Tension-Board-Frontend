2d5fbbf1ba8658b0460bf75ac0cd7fd5510934c6
Carlos Sandoval Olascoaga <ce.sandoval09@gmail.com>
2021-09-07 10:58:11 -0400

    initial svg

--- pages/index.vue
+++ pages/index.vue
@@ -1,17 +1,64 @@
 <template>
   <div class="index">
-    <img
-      src="/tension-board.png"
-      style="width: auto; height: 98vh;"
-      alt="My Image"
-    />
+    <div>
+      <svg
+        width="100vw"
+        height="100vh"
+        :style="{
+          position: 'absolute',
+        }"
+      >
+        <circle
+          v-for="(d, i) in data"
+          :key="i"
+          :cy="y(i)"
+          :cx="x(i)"
+          r="2.5vh"
+          :style="{
+            stroke: 'orange',
+            'stroke-width': '5px',
+            'fill-opacity': 0,
+          }"
+          @click="test(i)"
+        />
+      </svg>
+      <img
+        src="/tension-board.png"
+        style="width: auto; height: 98vh;"
+        alt="Tension"
+      />
+    </div>
   </div>
 </template>
 
 <script>
+const xSize = 3.46
+const ySize = 6.15
 export default {
   components: {
     //
+  },
+  data() {
+    return {
+      ledPos: [],
+      data: [...Array(11 * 15).keys()],
+    }
+  },
+  computed: {},
+  methods: {
+    x(i) {
+      return (i % 11) * xSize + xSize + 'vw' // 3.47 is the width of each line
+    },
+    y(i) {
+      return Math.floor(i / 11) * ySize + 2.7 + 'vh'
+    },
+    yCoord() {
+      return 1.9 + ySize + 'vh' // each line is 3vh
+    },
+    test(i) {
+      console.log('testestte' + i)
+      console.log(i % 11)
+    },
   },
 }
 </script>
